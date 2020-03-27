c8c1769eafc25d68949dbc4c4ed675ed64796d63
Christine Wong Chong <51415947+ChristineWC@users.noreply.github.com>
2020-03-27 11:06:38 -0700

    Update and rename Full Code to Full Code.c

--- Full Code.c
+++ Full Code.c
@@ -19,8 +19,7 @@ void mTimer(int count){
     //Set the Waveform Generation mode bit description to clear timer on compare math mode (CTC) only
     TCCR1B |=_BV(WGM12);
     OCR1A = 0x03e8; // sets the output compare register for 1000 cycles = 1ms
-    TCNT1 = 0x0000; //sets the initial value of the Timer Counter to 0x0000
-    TIMSK1 = TIMSK1 |0b00000010; // enables the output compare interrupt enable
+    TCNT1 = 0x0000; //sets the initial value of the Timer Counter to 0x00
     TIFR1 |=_BV(OCF1A); // so it checks whether the values of the timer counter 1 and the data of OCR1A are the same
     while(i < count){
         if((TIFR1 & 0x02) == 0x02){
