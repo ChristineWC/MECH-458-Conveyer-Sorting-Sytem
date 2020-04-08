3bef10b4189dd3f1415968374bbf34220fa5ff01
Christine Wong Chong <51415947+ChristineWC@users.noreply.github.com>
2020-04-07 20:30:34 -0700

    Update Steppertest2.c

--- Steppertest2.c
+++ Steppertest2.c
@@ -15,10 +15,10 @@ volatile int step_delay = 18; //global variable for i5s speed .
 volatile int dir = 1; //this should be either +1 or -1, set in step_what() to give direction for bucket
 volatile int dist = 0; //holds absolute value of # of steps to get where needs to go
 
-int steel_count = 0; 
-int aluminum_count = 0; 
-int black_count = 0; 
-int white_count = 0; 
+int steel_count = 0;
+int aluminum_count = 0;
+int black_count = 0;
+int white_count = 0;
 
 int pending_steel = 0;
 int pending_aluminum = 0;
@@ -28,10 +28,10 @@ int pending_white = 0;
 int spin[4] = {0b00110000, 0b00000110, 0b00101000, 0b00000101};
 
 unsigned int lowest;
-int ADC_resultflag = 0; 
+int ADC_resultflag = 0;
 
 const int Al_Max = 350;
-const int Al_Min = 0; 
+const int Al_Min = 0;
 const int St_Max = 699;
 const int St_Min = 351;
 const int Bl_Max = 1023;
@@ -39,7 +39,7 @@ const int Bl_Min = 941;
 const int Wh_Max = 940;
 const int Wh_Min = 700;
 
-List*list; 
+List*list;
 //Linked list functions
 
 List* new_list(){ //this is to initialize the list
@@ -136,21 +136,21 @@ Item* pop_front(List* this){ // use this function to delete stuff at the front o
 }
 
 
-//Functions 
+//Functions
 void mTimer(int count){
-    int i = 0;
-    //Set the Waveform Generation mode bit description to clear timer on compare math mode (CTC) only
-    TCCR1B |=_BV(WGM12);
-    OCR1A = 0x03e8; // sets the output compare register for 1000 cycles = 1ms
-    TCNT1 = 0x0000; //sets the initial value of the Timer Counter to 0x00
-    TIFR1 |=_BV(OCF1A); // so it checks whether the values of the timer counter 1 and the data of OCR1A are the same
-    while(i < count){
-        if((TIFR1 & 0x02) == 0x02){
-            TIFR1 |=_BV(OCF1A); //clears the interrupt flag by writing one to a bit
-            i++;
-        }
-    }
-    return;
+	int i = 0;
+	//Set the Waveform Generation mode bit description to clear timer on compare math mode (CTC) only
+	TCCR1B |=_BV(WGM12);
+	OCR1A = 0x03e8; // sets the output compare register for 1000 cycles = 1ms
+	TCNT1 = 0x0000; //sets the initial value of the Timer Counter to 0x00
+	TIFR1 |=_BV(OCF1A); // so it checks whether the values of the timer counter 1 and the data of OCR1A are the same
+	while(i < count){
+		if((TIFR1 & 0x02) == 0x02){
+			TIFR1 |=_BV(OCF1A); //clears the interrupt flag by writing one to a bit
+			i++;
+		}
+	}
+	return;
 }
 
 
@@ -159,15 +159,15 @@ void StepperGo(){
 	current_pos += dir;
 	
 	if (current_pos < 0)
-		current_pos = 199;
+	current_pos = 199;
 	
 	if(current_step == 4)
-		current_step = 0;
+	current_step = 0;
 	if(current_step == -1)
-		current_step = 3;
+	current_step = 3;
 	
 	PORTA = spin[current_step];
-	mTimer(step_delay);	
+	mTimer(step_delay);
 }
 
 void step_what(){ //sets the distance and speed
@@ -191,19 +191,19 @@ void step_what(){ //sets the distance and speed
 }
 
 void init_int() {//enables all necessary interrupts
-    // config the external interrupt ======================================
-    EIMSK |= (_BV(INT2)); // enable INT2 for OR sensor
-    EICRA |= (_BV(ISC21) | _BV(ISC20)); // rising edge interrupt for OR sensor
-    ADCSRA |= _BV(ADEN); // enable ADC
-    ADCSRA |= _BV(ADIE); // enable interrupt of ADC
-    ADMUX |= _BV(MUX0) | _BV(REFS0); //ADC Multiplexer selection register bits 5 and 6 set to 1, ADLAR = ADC left adjust result; 
-    //REFS0 set to 1 which selects voltage reference selection to core voltage (3.3v) 
-    EICRA |= _BV(ISC01);   // Falling Edge on INT0 for hall sensor
-    EIMSK |= _BV(INT0);    // Enable INT0 for hall sensor
-    EIMSK |= _BV(INT3); //Enable INT3 for EOT sensor	
-    EICRA |= _BV(ISC31); // falling edge interrupt for EOT sensor
-    EIMSK |= _BV(INT1); // Enable INT1 for the pause button
-    EICRA |= (_BV(ISC11) | _BV(ISC10)); // rising edge interrupt for pause button
+	// config the external interrupt ======================================
+	EIMSK |= (_BV(INT2)); // enable INT2 for OR sensor
+	EICRA |= (_BV(ISC21) | _BV(ISC20)); // rising edge interrupt for OR sensor
+	ADCSRA |= _BV(ADEN); // enable ADC
+	ADCSRA |= _BV(ADIE); // enable interrupt of ADC
+	ADMUX |= _BV(MUX0) | _BV(REFS0); //ADC Multiplexer selection register bits 5 and 6 set to 1, ADLAR = ADC left adjust result;
+	//REFS0 set to 1 which selects voltage reference selection to core voltage (3.3v)
+	EICRA |= _BV(ISC01);   // Falling Edge on INT0 for hall sensor
+	EIMSK |= _BV(INT0);    // Enable INT0 for hall sensor
+	EIMSK |= _BV(INT3); //Enable INT3 for EOT sensor
+	EICRA |= _BV(ISC31); // falling edge interrupt for EOT sensor
+	EIMSK |= _BV(INT1); // Enable INT1 for the pause button
+	EICRA |= (_BV(ISC11) | _BV(ISC10)); // rising edge interrupt for pause button
 }
 
 
@@ -215,22 +215,21 @@ ISR(INT0_vect) { // HE sensor is hooked up to PORTD0, will set current position 
 
 
 int main(){
-//initialization
-    DDRD = 0x00; // for the interrupts for the sensors
-    DDRB = 0xff; // for the dc motor
-    DDRC = 0xff; // output for the LCD
-    DDRA = 0xff; //output for stepper
-    DDRF = 0x00; //input for RL sensor @ F1
-    List* list = new_list();
-    
-    InitLCD(LS_BLINK|LS_ULINE); //initialize LCD subsystem
-    TCCR1B |=_BV(CS10); // we need this in main to use the timer
-    init_int(); //initializes all interrupts
-    PWM(); //Though the duty cycle may need to be changed for the DC motor
-    sei(); // sets the Global Enable for all interrupts
+	//initialization
+	DDRD = 0x00; // for the interrupts for the sensors
+	DDRB = 0xff; // for the dc motor
+	DDRC = 0xff; // output for the LCD
+	DDRA = 0xff; //output for stepper
+	DDRF = 0x00; //input for RL sensor @ F1
+	List* list = new_list();
+	
+	InitLCD(LS_BLINK|LS_ULINE); //initialize LCD subsystem
+	TCCR1B |=_BV(CS10); // we need this in main to use the timer
+	init_int(); //initializes all interrupts
+	sei(); // sets the Global Enable for all interrupts
 
 	
-//initialize the stepper to get it to the starting position
+	//initialize the stepper to get it to the starting position
 	LCDWriteStringXY(0, 0, "Homing Start");
 	while (current_pos != 0){
 		StepperGo();
@@ -239,7 +238,7 @@ int main(){
 	LCDWriteStringXY(0, 1, "Starting Sort");
 	mTimer(1500); //notifies that initialization is complete and ready to begin
 	
-//run a test, adding items to the linked list
+	//run a test, adding items to the linked list
 	
 	list->push_back(list, STEEL);
 	list->push_back(list, ALUMINUM);
@@ -247,39 +246,34 @@ int main(){
 	list->push_back(list, BLACK);
 
 	LCDClear();
-	LCDWriteStringXY(0, 0, "list size is: );
-	LCDWriteIntXY(14,0,list->size(), 2);
-
-
+	LCDWriteStringXY(0, 0, "list size is:" );
+	LCDWriteIntXY(14,0,list->size(list), 2);
 	
-	//loop stuff starts  
-  goto RUNNING;
-  
-  RUNNING:
-	  //output to lcd that it's running normally
-	PORTB = 0b00000010;//turns on DC motor forward (CCW)    
+	//loop stuff starts
+	goto RUNNING;
+	
+	RUNNING:
+	//output to lcd that it's running normally
+	PORTB = 0b00000010;//turns on DC motor forward (CCW)
 	
 	//Here we're gonna do some fucked shit to try to make this thing SMART
-		
+	
 	if((list->head->material != current_pos) && (list->head != NULL)){ //is the stepper/bucket ready to receive the next item?
 		step_what();//sets distance to go, and adjusts the step delay/stepper speed, and slows down belt if necessary
 		StepperGo();
 		
-	} else { // YES, in position
-		Item* it = list->head;
+		} else { // YES, in position
+	
 		Item* front = list->pop_front(list); // delete that item from the list
-		free(front); 
+		free(front);
 		LCDWriteIntXY(14,0,list->size(list), 2);
 		
 		step_delay = 18;
 		OCR0A |= 0b10000000; //sets duty cycle to 1/2 to speed belt back up after bucket aligned
-
 	}
-	  switch(current_state){
-	  	case(0):
-			goto RUNNING; //basically looping this stuff
-			break;
-
-	  } // Changes states, otherwise keeps running belt
-			 }
-
+	switch(current_state){
+		case(0):
+		goto RUNNING; //basically looping this stuff
+		break;
+	} // Changes states, otherwise keeps running belt
+}
