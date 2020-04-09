d972dd154d59068615ea45e0286b6f5d1388edf4
Christine Wong Chong <51415947+ChristineWC@users.noreply.github.com>
2020-04-09 12:30:02 -0700

    Update FULL_V3.c

--- FULL_V3.c
+++ FULL_V3.c
@@ -7,114 +7,75 @@
 
 //INCLUDE THE HEADER FILE FOR THE LIST
 
+//Global variables
+volatile int current_state = 0;//state (running or paused)
+volatile int current_step = 0;//for the stepper motor, (0-3)
+volatile int current_pos = 200;//current position of the bucket (0-199)
+volatile int step_delay = 18; //global variable for i5s speed .
+volatile int dir = 1; //this should be either +1 or -1, set in step_what() to give direction for bucket
+volatile int dist = 0; //holds absolute value of # of steps to get where needs to go
 
-int main(){
-	
-	link *head;			/* The ptr to the head of the queue */
-	link *tail;			/* The ptr to the tail of the queue */
-	link *newLink;		/* A ptr to a link aggregate data type (struct) */
-	link *DQ; 
+int steel_count = 0; 
+int aluminum_count = 0; 
+int black_count = 0; 
+int white_count = 0; 
 
-	newLink = NULL;
+int pending_steel = 0;
+int pending_aluminum = 0;
+int pending_black = 0;
+int pending_white = 0;
 
-	DDRA = 0x00; // port a is out input 
-	DDRC = 0xFF; // port c is our output 
+int spin[4] = {0b00110000, 0b00000110, 0b00101000, 0b00000101};
 
-	char readInput; 
- TCCR1B |=_BV(CS10);
-	/*
-	_BV sets the bit to logic 1
-	The register is TCCR1B0
-	TCCR1 is the Timer/counter control register 1
-	B is the B register and 0 is the 0th bit
-	CS means clock select (pre-scaler)
-	*/
+unsigned int lowest;
+int ADC_resultflag = 0; 
 
-while(1){
-setup(&head, &tail);
-PORTC = 0b00000000; 
+const int Al_Max = 350;
+const int Al_Min = 0; 
+const int St_Max = 699;
+const int St_Min = 351;
+const int Bl_Max = 1023;
+const int Bl_Min = 941;
+const int Wh_Max = 940;
+const int Wh_Min = 700;
 
-	while(size(&head, &tail) < 4){ //builds linked list of data
-			while ((PINA & 0x04) == 0x04){};
-			mTimer(20);
+//Linked list global
+Item* head;		
+Item* tail;
+Item* newLink; 
+Item* DQ;
 
-		
-		initLink(&newLink);
-		newLink->e.itemCode = PINA & 0x03; //putting the important bits in IC
-		enqueue(&head, &tail, &newLink); //linking it to the cue
-		while ((PINA & 0x04) == 0x00){};
-		mTimer(20);
 
-	}
+//Linked list functions
 
-	dequeue(&head, &DQ);
-	free(DQ);	
-
-	for(int i = 0; i < 3; i++){ //outputs data to leds
-		PORTC |= (head->e.itemCode) << (2*i); 
-			mTimer(2000);
-		dequeue(&head, &DQ);
-		free(DQ);
-	}
-
-			mTimer(7000);
-
-	//while((PINA & 0x04) == 0x00){}; 
-
-}
-
-	
-
-return (0); 
-}
-
-/**************************************************************************************
-* DESC: initializes the linked queue to 'NULL' status
-* INPUT: the head and tail pointers by reference
-*/
-
-void setup(link **h,link **t){
-	*h = NULL;		/* Point the head to NOTHING (NULL) */
-	*t = NULL;		/* Point the tail to NOTHING (NULL) */
+void setup(Item **h,Item **t){
+	*h = NULL;		
+	*t = NULL;		
 	return;
-}/*setup*/
+}
 
 /**************************************************************************************
 * DESC: This initializes a link and returns the pointer to the new link or NULL if error 
 * INPUT: the head and tail pointers by reference
 */
-void initLink(link **newLink){
+void initLink(Item **newLink){
 	//link *l;
 	*newLink = malloc(sizeof(link));
 	(*newLink)->next = NULL;
-	return;
-}/*initLink*/
+}
 
-/****************************************************************************************
-*  DESC: Accepts as input a new link by reference, and assigns the head and tail		
-*  of the queue accordingly				
-*  INPUT: the head and tail pointers, and a pointer to the new link that was created 
-*/
-/* will put an item at the tail of the queue */
-void enqueue(link **h, link **t, link **nL){
+/* push back */
+void enqueue(Item **h, Item **t, Item **nL){
 
-	if (*t != NULL){
-		/* Not an empty queue */
+	if (*t != NULL){ //if not empty
 		(*t)->next = *nL;
 		*t = *nL; //(*t)->next;
-	}/*if*/
-	else{
-		/* It's an empty Queue */
-		//(*h)->next = *nL;
-		//should be this
+	}
+	else{ // if empty 
 		*h = *nL;
 		*t = *nL;
-	}/* else */
-	return;
-}/*enqueue*/
-
-
-
+	}
+}
 
 /**************************************************************************************
 * DESC : Removes the link from the head of the list and assigns it to deQueuedLink
@@ -122,19 +83,24 @@ void enqueue(link **h, link **t, link **nL){
 * 		 which the removed link will be assigned to
 */
 /* This will remove the link and element within the link from the head of the queue */
-void dequeue(link **h, link **deQueuedLink){
-	/* ENTER YOUR CODE HERE */
+void dequeue(Item **h, Item **deQueuedLink){
+	/*
+	if (*head != NULL){ //This means that the list is not empty then 
+		//need to check if there is only one thing in which case head and tail go to null 
+		// if there is more than one thing 
+		// random pointer that is equal to the head so you don't lose the location of the head
+		// then make the head equal to the heads next 
+		// then take the pointer 
+	}
+	*/
+	
 	*deQueuedLink = *h;	// Will set to NULL if Head points to NULL
 	/* Ensure it is not an empty queue */
 	if (*h != NULL){
 		*h = (*h)->next;
-	}/*if*/
+	}
 	
-	return;
-}/*dequeue*/
-
-
-
+}
 
 /**************************************************************************************
 * DESC: Peeks at the first element in the list
@@ -142,62 +108,37 @@ void dequeue(link **h, link **deQueuedLink){
 * RETURNS: The element contained within the queue
 */
 /* This simply allows you to peek at the head element of the queue and returns a NULL pointer if empty */
-element firstValue(link **h){
-	return((*h)->e);
-}/*firstValue*/
+Material firstValue(Item **h){
+	return((*h)->mat);
+}
 
 
-
-
-
-/**************************************************************************************
-* DESC: deallocates (frees) all the memory consumed by the Queue
-* INPUT: the pointers to the head and the tail
-*/
 /* This clears the queue */
-void clearQueue(link **h, link **t){
+void clearQueue(Item **h, Item **t){
 
-	link *temp;
+	Item *temp;
 
 	while (*h != NULL){
 		temp = *h;
 		*h=(*h)->next;
 		free(temp);
-	}/*while*/
+	}
 	
 	/* Last but not least set the tail to NULL */
 	*t = NULL;		
 
 	return;
-}/*clearQueue*/
+}
 
 
-
-
-
-/**************************************************************************************
-* DESC: Checks to see whether the queue is empty or not
-* INPUT: The head pointer
-* RETURNS: 1:if the queue is empty, and 0:if the queue is NOT empty
-*/
-/* Check to see if the queue is empty */
-char isEmpty(link **h){
+char isEmpty(Item **h){
 	/* ENTER YOUR CODE HERE */
 	return(*h == NULL);
-}/*isEmpty*/
+}
 
+int size(Item **h, Item **t){
 
-
-
-/**************************************************************************************
-* DESC: Obtains the number of links in the queue
-* INPUT: The head and tail pointer
-* RETURNS: An integer with the number of links in the queue
-*/
-/* returns the size of the queue*/
-int size(link **h, link **t){
-
-	link 	*temp;			/* will store the link while traversing the queue */
+	Item 	*temp;			
 	int 	numElements;
 
 	numElements = 0;
@@ -210,4 +151,317 @@ int size(link **h, link **t){
 	}/*while*/
 	
 	return(numElements);
-}/*size*/
+}
+
+//Functions 
+void mTimer(int count){
+    int i = 0;
+    //Set the Waveform Generation mode bit description to clear timer on compare math mode (CTC) only
+    TCCR1B |=_BV(WGM12);
+    OCR1A = 0x03e8; // sets the output compare register for 1000 cycles = 1ms
+    TCNT1 = 0x0000; //sets the initial value of the Timer Counter to 0x00
+    TIFR1 |=_BV(OCF1A); // so it checks whether the values of the timer counter 1 and the data of OCR1A are the same
+    while(i < count){
+        if((TIFR1 & 0x02) == 0x02){
+            TIFR1 |=_BV(OCF1A); //clears the interrupt flag by writing one to a bit
+            i++;
+        }
+    }
+    return;
+}
+
+void init_int() {//enables all necessary interrupts
+    // config the external interrupt ======================================
+    EIMSK |= (_BV(INT2)); // enable INT2 for OR sensor
+    EICRA |= (_BV(ISC21) | _BV(ISC20)); // rising edge interrupt for OR sensor
+    ADCSRA |= _BV(ADEN); // enable ADC
+    ADCSRA |= _BV(ADIE); // enable interrupt of ADC
+    ADMUX |= _BV(MUX0) | _BV(REFS0); //ADC Multiplexer selection register bits 5 and 6 set to 1, ADLAR = ADC left adjust result; 
+    //REFS0 set to 1 which selects voltage reference selection to core voltage (3.3v) 
+    EICRA |= _BV(ISC01);   // Falling Edge on INT0 for hall sensor
+    EIMSK |= _BV(INT0);    // Enable INT0 for hall sensor
+    EIMSK |= _BV(INT3); //Enable INT3 for EOT sensor	
+    EICRA |= _BV(ISC31); // falling edge interrupt for EOT sensor
+    EIMSK |= _BV(INT1); // Enable INT1 for the pause button
+    EICRA |= (_BV(ISC11) | _BV(ISC10)); // rising edge interrupt for pause button
+}
+
+void PWM (){
+    TCCR0A |= 0b10000011; //first bit sets compare match output mode to clear, last two set whole thing to mode 3 (table 13-7)
+    TCCR0B |= 0b00000010; //sets clock prescale to 1/8 (Page 114)
+    OCR0A |= 0b10000000; //sets duty cycle to 1/2 
+}
+
+
+void StepperGo(){
+	current_step += dir;
+	current_pos += dir;
+	
+	if (current_pos < 0)
+		current_pos = 199;
+	
+	if(current_step == 4)
+		current_step = 0;
+	if(current_step == -1)
+		current_step = 3;
+	
+	PORTA = spin[current_step];
+	mTimer(step_delay);	
+}
+
+void step_what(){ //sets the distance and speed
+	dist = ((firstValue(&head)) - current_pos);
+	if (dist >= 100)
+	dist = dist - 200;
+	if (dist < -100)
+	dist = dist + 200;
+	dir = (dist)/ abs(dist);
+
+	//how far to go? set "acc_or_dec" based on current distance and step_delay
+	if (abs(dist) > 20 && (step_delay > 9) && (dist%2 == 0))
+	step_delay--;
+	if (abs(dist) < 16 && (step_delay < 18) && (dist%2 == 0))
+	step_delay++;
+	
+	//does the belt need to slow down?
+	if((PIND &= 0x08) == 0x08)// this means that there is something in front of the exit sensor
+	OCR0A |= 0b01000000; //sets duty cycle to 1/4 to slow down belt and allow bucket to prep
+
+}
+
+//ISRs
+
+ISR(ADC_vect){ //ISR for reflective sensor when ADC conversion complete 
+   // current_state = READ_IN_INT; -do we need this?
+    if(ADC < lowest) {
+        lowest = ADC;
+    }
+
+    if( (PIND & 0x04 ) == 0x04){ // if the item is still in front of the OR sensor 
+        ADCSRA |= _BV(ADSC); // Starts the conversion
+    }
+    else{
+	initLink(&newLink);
+	LCDClear(); 
+        
+	if(lowest <= Bl_Max && lowest >= Bl_Min){
+            newLink->mat = BLACK; 
+	    LCDWriteStringXY(0, 0, "PART: BLACK");
+        }
+        else if(lowest <= St_Max && lowest >= St_Min){
+            newLink->mat = STEEL; 
+	    LCDWriteStringXY(0, 0, "PART: STEEL");
+        }
+        else if(lowest <= Wh_Max && lowest >= Wh_Min){
+            newLink->mat = WHITE; 
+	    LCDWriteStringXY(0, 0, "PART: WHITE");
+        }
+        else if(lowest <= Al_Max && lowest >= Al_Min){
+            newLink->mat = ALUMINUM; 
+	    LCDWriteStringXY(0, 0, "PART: ALUMINUM");
+        }
+    }
+	LCDWriteStringXY(0, 1, "PART PENDING");
+}
+
+ISR(INT2_vect){ // OR sensor
+    lowest = 0xffff; 
+    ADCSRA |= _BV(ADSC); // Starts the conversion
+}
+
+ISR(INT3_vect){// EX/EOT sensor, it is hooked up to PORT D3
+	
+	if(mat == STEEL){
+		steel_count++;
+	}
+	else if(mat == ALUMINUM){
+		aluminum_count++;	
+	}
+	else if(mat == BLACK){
+		black_count++;	
+	}
+	else if(mat == WHITE){
+		white_count++; 
+	}
+	
+	dequeue(&head, &DQ);
+	free(DQ);
+	
+	LCDClear();
+	LCDWriteStringXY(0, 0, "PART SORTED");
+    /*
+    if((PIND &= 0x08) == 0x08)// this means that there is something in front of the exit sensor 
+    if((PIND &= 0x08) == 0x00)// this means that there is nothing in front of the exit sensor
+    */
+}
+
+
+
+ISR(INT0_vect) { // HE sensor is hooked up to PORTD0, will set current position to 0 every time HE sensor is triggered
+	current_pos = 0;
+}
+
+ISR(INT1_vect) { //pause button hooked up to D1
+	//put here whatever the pause button ISR code would be
+	mTimer(20);
+	if(current_state == 0){
+		current_state = 1;
+		}else{
+		current_state = 0;
+	}
+}
+
+int main(){
+//initialization
+    DDRD = 0x00; // for the interrupts for the sensors
+    DDRB = 0xff; // for the dc motor
+    DDRC = 0xff; // output for the LCD
+    DDRA = 0xff; //output for stepper
+    DDRF = 0x00; //input for RL sensor @ F1
+    List* list = new_list();
+    
+    InitLCD(LS_BLINK|LS_ULINE); //initialize LCD subsystem
+    TCCR1B |=_BV(CS10); // we need this in main to use the timer
+    init_int(); //initializes all interrupts
+    PWM(); //Though the duty cycle may need to be changed for the DC motor
+    sei(); // sets the Global Enable for all interrupts
+
+	
+//initialize the stepper to get it to the starting position
+	LCDWriteStringXY(0, 0, "Homing Start");
+	while (current_pos != 0){
+		StepperGo();
+	}
+	LCDWriteStringXY(0, 0, "Homing Complete");
+	LCDWriteStringXY(0, 1, "Starting Sort");
+	mTimer(1500); //notifies that initialization is complete and ready to begin
+	
+	
+	//loop stuff starts  
+  goto RUNNING;
+  
+  RUNNING:
+	  //output to lcd that it's running normally
+	PORTB = 0b00000010;//turns on DC motor forward (CCW)    
+	
+	//Here we're gonna do some fucked shit to try to make this thing SMART
+		
+	if(((firstValue(&head)) != current_pos) && (head != NULL)){ //is the stepper/bucket ready to receive the next item?
+		step_what();//sets distance to go, and adjusts the step delay/stepper speed, and slows down belt if necessary
+		StepperGo();
+		
+	} else { // YES, in position
+		step_delay = 18;
+		OCR0A |= 0b10000000; //sets duty cycle to 1/2 to speed belt back up after bucket aligned
+
+	}
+	  switch(current_state){
+	  	case(0):
+			goto RUNNING; //basically looping this stuff
+			break;
+		case(1):
+			goto PAUSE; //triggered by pause interrupt
+			break;
+
+	  } // Changes states, otherwise keeps running belt
+  	
+  PAUSE:
+  	//lcd output that we in the pause state
+	PORTB = 0b00000000;	//DC motor STOP
+	PORTB = 0b00001100; 	//DC motor OFF
+	step_delay = 17; //returns the step delay to a reasonable speed, in case it was paused in the bucket stage
+	//might need a small loop here that ramps down the bucket in case it's going full tilt
+	
+	//To count up the number and the types of things pending
+	Item* item = list-> head;
+	
+	while(item != NULL){ 
+	    if(item-> material == STEEL){
+		pending_steel++; 
+	    }
+	    else if(item->material == ALUMINUM){
+		pending_aluminum++;
+	    }
+	    else if(item->material == BLACK){
+		pending_black++;
+	    }
+	    else if(item->material == WHITE){
+		pending_white++;
+	    }
+	    item = item->next; 
+	}
+	
+	/*
+	Item 	*temp;			
+	temp = *h;			
+	while(temp != NULL){
+	   if(temp->mat == STEEL){
+	     	pending_steel++; 
+	    }
+	    else if(temp->mat == ALUMINUM){
+	     	pending_aluminum++; 
+	    }
+	    else if(temp->mat == BLACK){
+	     	pending_black++; 
+	     } 
+	     else if(temp->mat == WHITE){
+	     	pending_white++; 
+	     }
+		temp = temp->next;
+	}
+	*/
+	
+	
+	
+	//LCD displays number of sorted and pending items
+	LCDWriteStringXY(2, 0, "SYSTEM PAUSE");
+		for(int i = 0; i < 100; i++){
+			mTimer(15);
+			if(current_state == 0)
+			goto RUNNING;
+		}
+		LCDClear();
+
+	LCDWriteStringXY(0, 0, "P = Pending");
+	LCDWriteStringXY(0, 1, "S = Sorted");
+	for(int i = 0; i < 100; i++){
+		mTimer(10);
+		if(current_state == 0)
+		goto RUNNING;
+	}
+	LCDClear(); 	
+
+	//THIS FIRST PART IS SORTED ITEMS FROM WHEREVER WE STORE THEM
+	LCDWriteStringXY(0, 0, "BL: WH: ST: AL: ");
+	LCDWriteStringXY(0, 1, "S   S   S   S   S   ");
+	LCDWriteIntXY(1,1,black_count, 2);
+	LCDWriteIntXY(1,5,white_count, 2);
+	LCDWriteIntXY(1,9,steel_count, 2);
+	LCDWriteIntXY(1,13,aluminum_count, 2);
+	for(int i = 0; i < 100; i++){
+			mTimer(30);
+			if(current_state == 0)
+			goto RUNNING;
+	}
+	
+	//THIS NEXT PART IS PENDING ITEMS FROM LINKED LIST	
+	LCDWriteStringXY(0, 0, "BL: WH: ST: AL: ");
+	LCDWriteStringXY(0, 1, "P   P   P   P   P   ");
+	LCDWriteIntXY(1,1,pending_black, 2);
+	LCDWriteIntXY(1,5,pending_white, 2);
+	LCDWriteIntXY(1,9,pending_steel, 2);
+	LCDWriteIntXY(1,13,pending_aluminum, 2);
+	for(int i = 0; i < 100; i++){
+		mTimer(30);
+		if(current_state == 0)
+		goto RUNNING;
+	}
+	
+	if (current_state == 1)
+		goto PAUSE;
+	else
+		goto RUNNING;
+	
+    
+}
+
